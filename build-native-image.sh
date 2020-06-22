4750e9958ca97c8a77838b6cb74d90bab5f876f6
Iván López <lopezi@objectcomputing.com>
2020-06-22 17:57:26 +0200

    If running on CI connect to the appropriate postgres instance

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,6 @@
-./gradlew assemble
+if [ ! -z "${CI}" ]; then
+  ./gradlew -PdatabaseUrl='jdbc:postgresql://postgreshost:5432/devDb' assemble
+else
+  ./gradlew assemble
+fi
 native-image --no-server --no-fallback --class-path build/libs/micronaut-jooq-graal-*-all.jar
